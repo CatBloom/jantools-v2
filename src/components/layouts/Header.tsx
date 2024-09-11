00da31ab2e7b98c8c2f05ab3a7daa5221448f439
CatBloom <shomail.cat@gmail.com>
2024-09-11 22:36:12 +0900

    theme状態管理

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,13 +1,19 @@
-import { AppBar, Box, Toolbar, useColorScheme } from '@mui/material';
+import { AppBar, Box, Toolbar } from '@mui/material';
 import { MUIThemeSwitch } from '../buttons/MUIThemeSwitch';
 import { Link } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { themeState } from '../../state/themeState';
 import logo from '../../assets/titlelogo.png';
 
 export const Header = () => {
-  const { mode, setMode } = useColorScheme();
-  if (!mode) {
-    return null;
-  }
+  const [theme, setTheme] = useRecoilState(themeState);
+
+  const switchHandler = (event: { target: { checked: boolean } }) => {
+    const newTheme = event.target.checked ? 'dark' : 'light';
+    setTheme(newTheme);
+    sessionStorage.setItem('theme', newTheme);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -25,8 +31,8 @@ export const Header = () => {
           </Link>
           <MUIThemeSwitch
             sx={{ m: 1, ml: 'auto' }}
-            checked={mode !== null ? mode === 'dark' : false}
-            onChange={(event) => setMode(event.target.checked ? 'dark' : 'light')}
+            checked={theme === 'dark'}
+            onChange={switchHandler}
           />
         </Toolbar>
       </AppBar>
