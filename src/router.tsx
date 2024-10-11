58aac0a33e17668863349f1650ff0e85671704a3
CatBloom <shomail.cat@gmail.com>
2024-10-11 23:56:43 +0900

    import修正

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import { lazy } from 'react';
-import { Home } from './pages';
+import { Home } from './pages/Home';
 import { AppLayout, ContentLayout } from './components/layouts';
 const Detail = lazy(() => import('./pages/Detail'));
 
