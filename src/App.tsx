690c1804d19bb44160f942030bb1a05be302b14b
CatBloom <shomail.cat@gmail.com>
2024-10-12 00:07:39 +0900

    import修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import { useSetRecoilState } from 'recoil';
-import { loadingAtom, themeAtom } from './recoil/atoms';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
+import { useRecoilValue } from 'recoil';
+import { themeAtom } from './recoil/atoms';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { lightTheme, darkTheme } from './theme';
-import { useRecoilValue } from 'recoil';
 
 export const App = () => {
-  const setLoading = useSetRecoilState(loadingAtom);
   const mode = useRecoilValue(themeAtom);
-
-  // loading test
-  const handler = async () => {
-    setLoading(true);
-    await new Promise<void>((resolve) => {
-      setTimeout(() => {
-        console.log('fetch');
-        resolve();
-      }, 2000);
-    });
-    setLoading(false);
-  };
-
   return (
     <>
       <ThemeProvider theme={mode === 'dark' ? darkTheme : lightTheme}>
         <CssBaseline />
         <RouterProvider router={router} />
-        <button onClick={handler}>スピナーテスト</button>
       </ThemeProvider>
     </>
   );
