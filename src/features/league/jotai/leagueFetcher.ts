553f140238cb2a06e7a4b82009abca0dd6e5c6ac
CatBloom <shomail.cat@gmail.com>
2024-11-17 06:38:03 +0900

    signalを使用

--- src/features/league/jotai/leagueFetcher.ts
+++ src/features/league/jotai/leagueFetcher.ts
@@ -2,8 +2,8 @@ import { atom } from 'jotai';
 import { fetchLeague } from '../../../api/services/leagueService';
 import { paramWithIDAtom } from '../../../jotai/paramsAtom';
 
-export const leagueFetcher = atom(async (get) => {
+export const leagueFetcher = atom(async (get, { signal }) => {
   const leagueID = get(paramWithIDAtom);
-  if (!leagueID) return;
-  return await fetchLeague(leagueID);
+  if (!leagueID) return null;
+  return await fetchLeague(leagueID, signal);
 });
