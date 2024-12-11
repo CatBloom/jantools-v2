0465e488e6bddcaa9c9dfbe51cd81d50939c0cd8
CatBloom <shomail.cat@gmail.com>
2024-12-11 12:13:31 +0900

    eslint エラー修正

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 export default tseslint.config(
   { ignores: ['dist'] },
   js.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
   eslintConfigPrettier,
   {
     files: ['**/*.{ts,tsx}'],
