2196651e9273e1cbd21737c43d8b7d9fedb336d6
CatBloom <shomail.cat@gmail.com>
2024-12-28 18:24:18 +0900

    修正

--- src/hooks/useSetIDParam.ts
+++ src/hooks/useSetIDParam.ts
@@ -1,18 +1,13 @@
 import { useEffect } from 'react';
-import { useParams, useLocation } from 'react-router';
+import { useParams } from 'react-router';
 import { useSetAtom } from 'jotai';
 import { paramWithIDAtom } from '../jotai/paramsAtom';
 
 export const useSetIDParam = () => {
   const { id } = useParams();
-  const loaction = useLocation();
   const setParamWithID = useSetAtom(paramWithIDAtom);
 
-  // 現在と前回のIDを保管。location変更時にトリガーする
   useEffect(() => {
     setParamWithID(id);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loaction.pathname]);
-
-  return { id };
+  }, [id, setParamWithID]);
 };
