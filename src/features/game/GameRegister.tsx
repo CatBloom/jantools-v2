9a80ae80b461976074777a212f54aae50a4fb1bb
CatBloom <shomail.cat@gmail.com>
2025-01-30 01:29:03 +0900

    修正：ゲーム登録時にモーダルを閉じる

--- src/features/game/GameRegister.tsx
+++ src/features/game/GameRegister.tsx
@@ -1,5 +1,6 @@
 import { ModalContainer } from '@/components/ModalContainer';
 import { LeagueRule } from '@/types/league';
+import { GameFormData } from '@/types/game';
 import { GameForm } from './components/GameForm';
 import { useGameData } from './hooks/useGameData';
 import { useGame } from './hooks/useGame';
@@ -10,9 +11,14 @@ export const GameRegister = (props: { rule: LeagueRule; isOpen: boolean; close: 
   const { playersData } = useGameData();
   const { create } = useGame();
 
+  const submit = (formdata: GameFormData) => {
+    create(formdata);
+    close();
+  };
+
   return (
     <ModalContainer modalTitle="成績登録" isOpen={isOpen} close={close}>
-      <GameForm rule={rule} gamePlayers={playersData} submit={create} />
+      <GameForm rule={rule} gamePlayers={playersData} submit={submit} />
     </ModalContainer>
   );
 };
