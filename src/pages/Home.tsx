43814a3bc06ebc68d9adf5ec99fc086e16b203d9
CatBloom <shomail.cat@gmail.com>
2025-05-19 03:08:24 +0900

    TOPページデザイン修正

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,22 +8,34 @@ export const Home = () => {
   return (
     <>
       <Box
-        component="img"
-        src="https://static.catbloom.net/jantools_top_image.webp"
-        alt="Logo"
         sx={{
           width: 1,
+          height: { xs: 'auto', md: 430 },
+          position: 'relative',
         }}
-      ></Box>
+      >
+        <Box
+          component="img"
+          src="https://static.catbloom.net/jantools_top_image.webp"
+          alt="Logo"
+          sx={{
+            width: 1,
+            height: { xs: 'auto', md: '100%' },
+            objectFit: { xs: 'contain', md: 'cover' },
+            objectPosition: 'top',
+          }}
+        />
+      </Box>
+
       <Container maxWidth="md">
-        <Stack spacing={3} mt={2}>
+        <Stack spacing={3} mt={4}>
           <Typography variant="h1">雀Tools</Typography>
           <Typography component="p">
             麻雀の成績管理アプリです。
             <br />
             大会登録を行い、成績を入力することで、全体の成績やプレイヤーごとの成績を簡単に管理できます。
           </Typography>
-          <LeagueRegister addFavorite={addFavorite}></LeagueRegister>
+          <LeagueRegister addFavorite={addFavorite} />
         </Stack>
       </Container>
     </>
