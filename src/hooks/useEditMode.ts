0feff2fb0af9f52fbd6897631eefec78d274d0b3
CatBloom <shomail.cat@gmail.com>
2025-06-01 04:10:50 +0900

    pathが変わった際のフラグ管理

--- src/hooks/useEditMode.ts
+++ src/hooks/useEditMode.ts
@@ -1,9 +1,12 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useNotice } from '@/hooks/useNotice';
+import { useLocation } from 'react-router';
 
 export const useEditMode = () => {
   const [isEdit, setIsEdit] = useState(false);
   const { set } = useNotice();
+  const location = useLocation();
+  const prevPathnameRef = useRef(location.pathname);
 
   const toggle = useCallback(() => {
     setIsEdit((prev) => {
@@ -15,6 +18,14 @@ export const useEditMode = () => {
       return next;
     });
   }, [set]);
+
+  // pathが変わった際に、editModeを解除する
+  useEffect(() => {
+    if (prevPathnameRef.current !== location.pathname) {
+      setIsEdit(false);
+      prevPathnameRef.current = location.pathname;
+    }
+  }, [location.pathname]);
 
   return { isEdit, setIsEdit, toggle };
 };
